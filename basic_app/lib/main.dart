71f657d4352e5eb1948453aaf448b622bf343347
Sazid Mahmud <sazidmahmud36@gmail.com>
2025-12-24 03:12:55 +0600

    Basic File
    main.dart (Basic Code)

---
+++ basic_app/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeActivity(),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Basic App"),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: Center(
+        child: Text("Hello Flutter Basic"),
+      ),
+    );
+  }
+}
