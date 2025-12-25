0cd58f73ed7717d85c324420caf050c2382f36fc
Sazid Mahmud <sazidmahmud36@gmail.com>
2025-12-26 02:29:21 +0600

    Row

--- basic_wiz/lib/main.dart
+++ basic_wiz/lib/main.dart
@@ -143,17 +143,24 @@ class HomeActivity extends StatelessWidget{
           ],
         ),
       ),
-      body: Container(
-        height: 250,
-        width: 250,
-        child: Image.network("https://avatars.githubusercontent.com/u/45362238?v=4"),
-        alignment: Alignment.center,
-        margin: EdgeInsets.fromLTRB(50, 20, 50, 20),
-        decoration: BoxDecoration(
-          color: Colors.amber,
-          border: Border.all(color: Colors.black, width: 6),
-        ),
-      ),
+      // body: Container(
+      //   height: 250,
+      //   width: 250,
+      //   child: Image.network("https://avatars.githubusercontent.com/u/45362238?v=4"),
+      //   alignment: Alignment.center,
+      //   margin: EdgeInsets.fromLTRB(50, 20, 50, 20),
+      //   decoration: BoxDecoration(
+      //     color: Colors.amber,
+      //     border: Border.all(color: Colors.black, width: 6),
+      //   ),
+      // ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(height: 100, width: 100,child: Image.network("https://avatars.githubusercontent.com/u/45362238?v=4"),),
+          Container(height: 100, width: 100,child: Image.network("https://avatars.githubusercontent.com/u/45362238?v=4"),),
+          Container(height: 100, width: 100,child: Image.network("https://avatars.githubusercontent.com/u/45362238?v=4"),),
+      ],),
     );
   }
 }
