2862c283320077058c55364179cb2867cdfb3f3b
Sazid Mahmud <sazidmahmud36@gmail.com>
2026-01-02 13:01:35 +0600

    Simple Navigation

---
+++ simple_navigation/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeActivity());
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Home"),
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
+        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          ElevatedButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>ActivityOne()));
+          }, child: Text("Activity 1")),
+          ElevatedButton(onPressed: (){}, child: Text("Activity 2")),
+        ],
+      ),
+    );
+  }
+}
+class ActivityOne extends StatelessWidget {
+  const ActivityOne({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("ActivityOne"),
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
+        ),
+      body: Center(
+        child: Text("ActivityOne"),
+      ),
+    );
+  }
+}
+class ActivityTwo extends StatelessWidget {
+  const ActivityTwo({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("ActivityTwo"),
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
+        ),
+      body: Center(
+        child: Text("ActivityTwo"),
+      ),
+    );
+  }
+}
