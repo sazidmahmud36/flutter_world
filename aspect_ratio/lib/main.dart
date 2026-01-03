0a129c917cdbd1f102e1f9ce06e3498de0b4ccf5
Sazid Mahmud <sazidmahmud36@gmail.com>
2026-01-03 22:34:43 +0600

    Aspect Ratio + Fractionally Sized Box

---
+++ aspect_ratio/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeActivity(),
+    );
+  }
+}
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Aspect Ratio"),backgroundColor: Colors.pink,foregroundColor: Colors.white,),
+      // ==================== AspectRatio ==================
+
+      // body: Container(
+      //   width: double.infinity,
+      //   height: 300,
+      //   color: Colors.red,
+      //   alignment: Alignment.center,
+      //   child: AspectRatio(
+      //     aspectRatio: 16/9,
+      //     child: Container(
+      //       color: Colors.green,
+      //     ),
+      //   ),
+      // ),
+
+      // ==================== FractionallySizedBox ==================
+
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          heightFactor: 0.5,
+          child: Container(
+            color: Colors.green,
+          ),
+        ),
+      ),
+    );
+  }
+
+}
